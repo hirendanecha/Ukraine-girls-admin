cddd345946240b3e94b530429a6d81841812251b
ppa-opash <ppa.opash@gmail.com>
2024-07-26 11:51:12 +0530

    initial commit

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  hmr: false,
+  serverUrl: 'https://dev-api.ukrainiangirls.tube/api/v1/',
+  serverUrl2: 'http://localhost:8080/api/v1/',
+  socketUrl: 'https://dev-api.ukrainiangirls.tube/',
+  socketUrl2: 'http://localhost:8080/',
+  imgUrl: 'root/storage/uploads',
+};
